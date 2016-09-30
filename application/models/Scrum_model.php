cd355ec6366e7672d4000e3bd9787950c7ef9c3d
jpyadav <jpyadav.sa@gmail.com>
2016-09-30 19:32:56 +0530

    Update Scrum_model.php

--- application/models/Scrum_model.php
+++ application/models/Scrum_model.php
@@ -1,56 +1,50 @@
 <?php
-
 class Scrum_model extends CI_Model {
 
-private $board_id;
+        private $board_id;
+		
         public function __construct()
         {
-                $this->load->database();
-                $this->load->library('session');
-                $ci=&get_instance();
-        $db =$ci->load->database('pm',true);
-          $this->load->model('Log_model');
+                //$this->load->database();
+                //$this->load->library('session');
+                //$ci=&get_instance();
+                //$db =$ci->load->database('pm',true);
+                $this->load->model('Log_model');
+				date_default_timezone_set("Asia/Calcutta");
 
         }
-function add_board($board_name,$board_team,$cur_board_id){
-
- $ci=&get_instance();
-        $db =$ci->load->database('pm',true);
-        //echo $cur_board_id;
-        //die;
-        $query1=$db->query("select * from project_board where board_id=".$cur_board_id)->row();
-
-        if(isset($query1))
-        {
-          $proj_id=$query1->project_id;
-        }
-// $this->session->userdata('proj_id');
-$user_id=$this->session->userdata('USER_ID');
-if($board_team!='')
-$team=$user_id.",".$board_team;
-else
-$team=$user_id;
-date_default_timezone_set("Asia/Calcutta");
-$date       = date("Y-m-d");
-
-    $data = array(
-   'board_name' => $board_name ,
-   'project_id' => $proj_id ,
-        'start_date' => $date,
-        'added_by' =>$user_id,
-        'board_team'=>$team
-
-
-);
-
-if($db->insert('project_board', $data))
-{
-    $board_id=$db->insert_id();
-return $board_id;
-}
-else
-return $cur_board_id."failure".$proj_id.$query1; 
+		
+		
+		/*function add_task(){
+	     
+		     $sql_query = 'insert into table_name(new_card_id, task_name, assign_to, task_desc, pri_task) values(?,?,?,?,?)';
+		      $this->db->query($sql_query,array($this->new_card_id, $this->task_name, $this->assign_to, $this->task_desc,$this->pri_task));
+		    return ($this->db->affected_rows() != 1) ? false : true;	
+		}*/
+		
+		
+		function move_task(){
+	       
+		     $sql_query = 'insert into table_name(task_id, new_user_name, card_id) values(?,?,?)';
+		      $this->db->query($sql_query,array($this->task_id, $this->new_user_name, $this->card_id));
+		    return ($this->db->affected_rows() != 1) ? false : true;	
+		
+		
+		}
+		
+		
+		
+		function add_task(){
+	         $ip = $this->input->ip_address();
+		     $sql_query = 'insert into project_details(proj_name, uid, project_team, dept, start_date, deadline, ip, added_by) values(?,?,?,?,?,?,?,?)';
+		      $this->db->query($sql_query,array($this->proj_name, $this->uid, $this->project_team, $this->dept,$this->start_date, $this->deadline, $ip, $this->added_by));
+		    return ($this->db->affected_rows() != 1) ? false : true;	
+		}
+		
+		
+		
 
 }
-}
-?> 
+
+
+
